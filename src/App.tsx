372845170bf9c5d7e1a1d9d9e7b3f03216abf718
Brady <bradyryun@gmail.com>
2024-01-07 17:00:16 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { LevelCreator } from './components/level-creator';
+import AlertComponent from './context/alert-component';
+import { AlertProvider } from './context/alert-provider';
+import './index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AlertProvider>
+      <AlertComponent />
+      <LevelCreator />
+    </AlertProvider>
   );
 }
 
